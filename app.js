1d14c8efbeb224e3d73b490d1903e21d0d316541
Rachel Hamlin <rachelmhamlin@gmail.com>
2016-05-27 22:30:36 -0400

    sets up angular routing; adds bg color

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var app = express();
 
 app.use(express.static( __dirname + '/public' ));
 
-app.get('/', function( req, res, next ) {
+var indexRouter = function(req, res){
   res.sendFile( __dirname + '/public/views/index.html' );
-});
+}
+
+app.get('/', indexRouter);
+app.get('*', indexRouter);
 
 var port = 8080;
 app.listen(port, function(){
--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,21 @@
 console.log('hey girl, whats up, its the end of the day');
 
-// var app = angular.module('MoodApp', ['ngRoute']);
-//
-// app.config(['$routeProvider', function( $routeProvider ) {
-//   $routeProvider
-//     .when('/results', {
-//       templateUrl: '/views/partials/results.html',
-//       controller: 'resultsController'
-//     })
-//
-//     .otherwise({
-//       redirectTo: '/'
-//     })
-//
-// }]);
-//
-//
-// app.controller('inputController', ['$scope', function( $scope ) {
-//   $scope.variable = 'string';
-// }]);
+var app = angular.module('MoodApp', ['ngRoute']);
+
+app.config(['$routeProvider', function( $routeProvider ) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '/views/partials/input.html',
+      controller: 'inputController'
+    })
+
+    .when('/results', {
+      templateUrl: '/views/partials/results.html',
+      controller: 'resultsController'
+    })
+
+    .otherwise({
+      redirectTo: '/index'
+    })
+
+}]);
