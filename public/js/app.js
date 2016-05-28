0f65cd2e688fbe2c8ccbda2737597d6aeb4a50b4
Rachel Hamlin <rachelmhamlin@gmail.com>
2016-05-27 21:02:59 -0400

    adds grid

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,21 @@
 console.log('hey girl, whats up, its the end of the day');
+
+// var app = angular.module('MoodApp', ['ngRoute']);
+//
+// app.config(['$routeProvider', function( $routeProvider ) {
+//   $routeProvider
+//     .when('/results', {
+//       templateUrl: '/views/partials/results.html',
+//       controller: 'resultsController'
+//     })
+//
+//     .otherwise({
+//       redirectTo: '/'
+//     })
+//
+// }]);
+//
+//
+// app.controller('inputController', ['$scope', function( $scope ) {
+//   $scope.variable = 'string';
+// }]);
