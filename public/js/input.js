122abb287fc7f2aafafde5f1648c33b448ee1f82
Rachel Hamlin <rachelmhamlin@gmail.com>
2016-05-28 18:25:43 -0400

    scope issues; multiple views same controller

--- public/js/input.js
+++ public/js/input.js
@@ -1,9 +1,11 @@
 app.controller('inputController', ['$scope', '$http', '$location', function( $scope, $http, $location ) {
 
    $scope.sendInput = function(textInput) {
-     $http.get('http://gateway-a.watsonplatform.net/calls/text/TextGetEmotion?apikey=397940cb2b4f38eae9ec9886d64ae850e520c4f7&text=' + textInput).then(function(response){
-       console.log(response.data);
-       $location.path('/results')
+     $http.get('http://gateway-a.watsonplatform.net/calls/text/TextGetEmotion?apikey=397940cb2b4f38eae9ec9886d64ae850e520c4f7&outputMode=json&text=' + textInput).then(function(response){
+       var emotions = response.data.docEmotions;
+       $scope.anger = emotions.anger;
+       console.log($scope.emotions);
+      //  $location.path('/results');
      })
    }
 
