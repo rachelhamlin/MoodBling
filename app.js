1823344fcfaf52d5d5a5419a0ef0aafaca9d6036
Rachel Hamlin <rachelmhamlin@gmail.com>
2016-05-31 09:53:30 -0400

    updates style and partials

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ var indexRouter = function(req, res){
 
 
 var env = process.env.NODE_ENV || 'develop';
+// var watson_key = process.env.WATSON_KEY;
+var watson_key = '397940cb2b4f38eae9ec9886d64ae850e520c4f7';
+console.log('hey this is my watson key' + watson_key);
 
 app.get('/config', function (req, res) {
   if(env === 'develop') {
@@ -35,3 +38,5 @@ var port = 8080;
 app.listen(port, function(){
   console.log('listening on 8080, my favorite port');
 })
+
+module.exports = watson_key;
--- public/js/services/alchemy.js
+++ public/js/services/alchemy.js
@@ -3,7 +3,7 @@ app.factory('alchemyAPI', ['$http', '$rootScope', '$timeout', function( $http, $
   var alchemy = {};
 
   alchemy.getAnalysis = function(textInput) {
-    return $http.get('http://gateway-a.watsonplatform.net/calls/text/TextGetEmotion?apikey=397940cb2b4f38eae9ec9886d64ae850e520c4f7&outputMode=json&text=' + textInput).then(function(response){
+    return $http.get('http://gateway-a.watsonplatform.net/calls/text/TextGetEmotion?apikey=' + watson_key + '&outputMode=json&text=' + textInput).then(function(response){
       var emotions = response.data.docEmotions;
       console.log(emotions);
       console.log(emotions.anger);
